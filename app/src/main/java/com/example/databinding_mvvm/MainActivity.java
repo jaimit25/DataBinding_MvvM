54806338b6d9b39cfd4161c7924495e5cede3526
Jaimit <jaimit.jp@gmail.com>
2022-08-12 21:50:41 +0530

    MVVM

--- app/src/main/java/com/example/databinding_mvvm/MainActivity.java
+++ app/src/main/java/com/example/databinding_mvvm/MainActivity.java
@@ -2,19 +2,39 @@ package com.example.databinding_mvvm;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
 
 import com.example.databinding_mvvm.databinding.ActivityMainBinding;
+import com.example.databinding_mvvm.models.Customer;
+import com.example.databinding_mvvm.viewmodels.MainviewModel;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding mainxml;
 
+    //Create object of mainViewModel(provider class which will provide data)
+    MainviewModel mainviewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mainxml = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+//        Directly using model class
+//        Customer customer = new Customer("Hotmarty");
+//        mainxml.setCustomer(customer);
+
+
+//        Getting data from provider class instead of directly accessing data frm the model class
+//        Model class will act as intermediate for passing the Data
+
+        mainviewModel = ViewModelProviders.of(this).get(MainviewModel.class);
+        mainxml.setCustomer(mainviewModel.getCustomer());
+
 
     }
 
---
+++ app/src/main/java/com/example/databinding_mvvm/models/Customer.java
@@ -0,0 +1,18 @@
+package com.example.databinding_mvvm.models;
+
+public class Customer {
+
+    String cname;
+
+    public Customer(String cname) {
+        this.cname = cname;
+    }
+
+    public String getCname() {
+        return cname;
+    }
+
+    public void setCname(String cname) {
+        this.cname = cname;
+    }
+}
