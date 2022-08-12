ffa8a5708abb4c90ce210f1351bc8b3f250424e8
Jaimit <jaimit.jp@gmail.com>
2022-08-12 21:26:06 +0530

    updates

--- app/src/main/java/com/example/databinding_mvvm/MainActivity.java
+++ app/src/main/java/com/example/databinding_mvvm/MainActivity.java
@@ -11,25 +11,10 @@ public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding mainxml;
 
-    //Approach2 : model class name should be Capital
-//    Product product;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
         mainxml = DataBindingUtil.setContentView(this,R.layout.activity_main);
-
-
-
-        //Approach1 : txt1 is id of the textview also we don't need to initialise the variable for the textview
-        //mainxml.txt1.setText("This is Main Activity Page"); // same as ViewBinding
-
-        //Approach2 : using model class
-//        Product product = new Product("this is product 1 "," this is random text 2");
-//        mainxml.setProduct(product); // setProduct is automatically created by the Binding class
-
 
     }
 
